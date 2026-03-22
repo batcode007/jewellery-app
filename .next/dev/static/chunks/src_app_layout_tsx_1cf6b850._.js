152146f79d78e3d33ab049f7467e5f3f22c35ca9
Naman Gupta <naman.gupta08@gmail.com>
2026-03-22 13:13:29 +0530

    Delete .next directory

--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__f6202ce6._.css",
-  "static/chunks/node_modules_25609bbe._.js",
-  "static/chunks/src_fc1ab40d._.js"
-],
-    source: "dynamic"
-});
